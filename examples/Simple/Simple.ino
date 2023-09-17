4ff4bf7859287c4adeef3f2ec171fe44105785ed
Chris Mullins <chris@sidoh.org>
2023-09-17 11:05:55 -0700

    Use std::shared_ptr and create a copy of the path

--- examples/Simple/Simple.ino
+++ examples/Simple/Simple.ino
@@ -9,8 +9,8 @@ void setup() {
   char p1[] = "example/path/a/b/c";
   char p2[] = "example/path/:var1/:var2/:var3";
 
-  TokenIterator itr1(p1, strlen(p1), '/');
-  TokenIterator itr2(p2, strlen(p2), '/');
+  auto itr1 = std::make_shared<TokenIterator>(p1, strlen(p1), '/');
+  auto itr2 = std::make_shared<TokenIterator>(p2, strlen(p2), '/');
   UrlTokenBindings bindings(itr1, itr2);
 
   if (bindings.hasBinding("var1")) {      // has this one
--- src/UrlTokenBindings.h
+++ src/UrlTokenBindings.h
@@ -1,18 +1,23 @@
 #include <TokenIterator.h>
 
+#include <memory>
+
 #ifndef _URL_TOKEN_BINDINGS_H
 #define _URL_TOKEN_BINDINGS_H
 
 class UrlTokenBindings {
 public:
-  UrlTokenBindings(TokenIterator& patternTokens, TokenIterator& requestTokens);
+  UrlTokenBindings(
+      std::shared_ptr<TokenIterator> patternTokens,
+      std::shared_ptr<TokenIterator> requestTokens
+  );
 
   bool hasBinding(const char* key) const;
   const char* get(const char* key) const;
 
 private:
-  TokenIterator& patternTokens;
-  TokenIterator& requestTokens;
+  std::shared_ptr<TokenIterator> patternTokens;
+  std::shared_ptr<TokenIterator> requestTokens;
 };
 
 #endif
